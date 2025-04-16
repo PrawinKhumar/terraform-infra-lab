8462d4fc127e19c459d7cc1dbb98c6005f6d8e34
PrawinKhumar <prawin.selvaraj@spireminds.com>
2025-04-16 13:29:49 +0100

    Fix Terraform config errors: remove invalid lines and duplicate variables

--- dev/main.tf
+++ dev/main.tf
@@ -6,13 +6,4 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
-dev/variables.tf
-hcl
-CopyEdit
-variable "resource_group_name" {
-  default = "dev-rg"
-}
 
-variable "location" {
-  default = "East US"
-}
