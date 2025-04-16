1973cc728eb10faeec66b338eaf09cccffb3c983
PrawinKhumar <prawin.selvaraj@spireminds.com>
2025-04-16 14:14:07 +0100

    Refactor provider to read credentials from env vars

--- dev/main.tf
+++ dev/main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
-subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+ subscription_id = env("ARM_SUBSCRIPTION_ID")
+  tenant_id       = env("ARM_TENANT_ID")
+  client_id       = env("ARM_CLIENT_ID")
+  client_secret   = env("ARM_CLIENT_SECRET")
 }
 
 resource "azurerm_resource_group" "rg" {
