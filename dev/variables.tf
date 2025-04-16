97d7217de61a530b07adc5b822f9c0854948f854
PrawinKhumar <prawin.selvaraj@spireminds.com>
2025-04-16 13:54:12 +0100

    Creds Edited

--- dev/variables.tf
+++ dev/variables.tf
@@ -2,6 +2,11 @@ variable "resource_group_name" {
   default = "dev-rg"
 }
 
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+
 variable "location" {
   default = "East US"
 }
